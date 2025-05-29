bbe0800a12e2b814c6c49ab74fd1861971caaf74
robisoncrisbrito <robison.brito@gmail.com>
2025-05-29 20:49:13 -0300

    Interface Gráfica pronta + incluir

--- app/src/main/java/br/edu/utfpr/usandodb_pm45s_2025_1/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/usandodb_pm45s_2025_1/MainActivity.kt
@@ -1,9 +1,12 @@
 package br.edu.utfpr.usandodb_pm45s_2025_1
 
+import android.content.ContentValues
 import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
+import androidx.annotation.ContentView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -24,11 +27,19 @@ class MainActivity : AppCompatActivity() {
 
         banco = openOrCreateDatabase("dbfile.sqlite", MODE_PRIVATE, null)
         banco.execSQL("CREATE TABLE IF NOT EXISTS cadastro (_id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, telefone TEXT)")
-
     }
 
     fun btIncluirOnClick(view: View) {
+        val registro = ContentValues()
+        registro.put("nome", etNome.text.toString())
+        registro.put("telefone", etTelefone.text.toString())
+        banco.insert("cadastro", null, registro)
 
+        Toast.makeText(
+            this,
+            "Inclusão realizada com sucesso!",
+            Toast.LENGTH_SHORT
+        ).show()
     }
     fun btAlterarOnClick(view: View) {
 
