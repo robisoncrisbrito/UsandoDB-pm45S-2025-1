40d70e0144e189f989a6031e341fd261434e3abd
robisoncrisbrito <robison.brito@gmail.com>
2025-05-29 20:12:00 -0300

    Interface Gráfica pronta + parcial do código kotlin

--- app/src/main/java/br/edu/utfpr/usandodb_pm45s_2025_1/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/usandodb_pm45s_2025_1/MainActivity.kt
@@ -2,17 +2,38 @@ package br.edu.utfpr.usandodb_pm45s_2025_1
 
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var etCod: EditText
+    private lateinit var etNome: EditText
+    private lateinit var etTelefone: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etCod = findViewById(R.id.etCod)
+        etNome = findViewById(R.id.etNome)
+        etTelefone = findViewById(R.id.etTelefone)
+
     }
 
-    fun btIncluirOnClick(view: View) {}
-    fun btAlterarOnClick(view: View) {}
-    fun btExcluirOnClick(view: View) {}
-    fun btPesquisarOnClick(view: View) {}
-    fun btListarOnClick(view: View) {}
+    fun btIncluirOnClick(view: View) {
+
+    }
+    fun btAlterarOnClick(view: View) {
+
+    }
+    fun btExcluirOnClick(view: View) {
+
+    }
+    fun btPesquisarOnClick(view: View) {
+
+    }
+    fun btListarOnClick(view: View) {
+
+    }
 }
